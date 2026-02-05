726e2e55a3f8d6548b80b190a1304a349cbaa560
Angela Caperuzzo <79520916+AngelaCaperuzzo@users.noreply.github.com>
2026-02-05 17:39:21 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,7 @@
+nome = input('Qual o seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input ('Quanto é o seu peso?')
+
+print('Olá, '+nome)
+print('Vejo que sua idade é de '+idade+ ' anos.')
+print('E seu peso é de '+peso+'kg.')
