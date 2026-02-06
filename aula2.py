7b45d05c0fae605a605ee80fdd2aca7b59cf0038
Angela Caperuzzo <79520916+AngelaCaperuzzo@users.noreply.github.com>
2026-02-06 16:39:32 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,6 @@
+n1 = int( input('Digite aqui um número: '))
+n2 =  int(input('Digite aqui outro número: '))
+r = n1+n2
+
+print('Você sabia que a soma desses dois números digitados é de: ', r)
+print('Veja: {} + {} = {}'.format(n1,n1,r))
