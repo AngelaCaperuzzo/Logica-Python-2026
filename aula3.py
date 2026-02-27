9e32c2a5a2f536595543ae7f2e1a44300134d98d
Angela Caperuzzo <79520916+AngelaCaperuzzo@users.noreply.github.com>
2026-02-27 11:18:02 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,3 @@
+print('Aprendi sobre argumentos nomeados.', 'O famoso separador.', sep='*')
+print('Também aprendi sobre o final de linha.', 'O famoso END.', end="=")
+print("\nTambém\naprendi\na\nquebra de linha")
