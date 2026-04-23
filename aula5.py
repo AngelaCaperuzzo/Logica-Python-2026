9d636b9a5ed206c01e292cb1271f3db971d5f1e5
Angela Caperuzzo <79520916+AngelaCaperuzzo@users.noreply.github.com>
2026-04-23 19:35:26 -0300

    calculo imc com formatação f-string

---
+++ aula5.py
@@ -0,0 +1,11 @@
+nome = 'Angela Caperuzzo'
+altura = 1.56
+peso = 82
+imc = peso / (altura * altura)
+
+linha_1 = f'{imc:.2f}'
+
+print('Nome: ', nome)
+print('Altura: ', altura)
+print('Peso: ', peso)
+print('Seu IMC: ', linha_1)
