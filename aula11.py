53b9f1fadb6ebcd1e4f30d935bcfebb4d820d743
Angela Caperuzzo <79520916+AngelaCaperuzzo@users.noreply.github.com>
2026-05-19 10:41:33 -0300

    Operadores Lógicos

---
+++ aula11.py
@@ -0,0 +1,10 @@
+nome = input('Qual o seu primeiro nome?: ')
+
+if len(nome) <= 4:
+    print('Seu nome é curto.')
+
+elif 5 <= len(nome) <= 6:
+    print('Seu nome é normal.')
+
+else:
+    print('Seu nome é muito grande.')
